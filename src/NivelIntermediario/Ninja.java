0a1d0a0f362354c03909693a0a5322d50e60e277
ViniciusLgo <vinicruzlago@gmail.com>
2024-11-09 15:44:45 -0300

    Update Ninja.java

--- src/NivelIntermediario/Ninja.java
+++ src/NivelIntermediario/Ninja.java
@@ -11,7 +11,8 @@ public abstract class Ninja implements EstrategiaDeBatalha {
         System.out.println("Meu nome é " + nome + " e esse é meu ataque especial");
     }
 
-    // Sobreescrevendo o metodo da interface
+
+    //  Sobreescrevendo o metodo da interface
     @Override
     public void estrategiaDeBatalhaNinja() {
         System.out.println( "Meu nome é: " + nome + " Essa é minha estrategia de combate");
