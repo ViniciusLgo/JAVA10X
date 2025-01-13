14457b16a56aea590d29e7f8903ae1646630bca6
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-01-13 16:28:35 -0300

    update herança

--- src/NivelIntermediario/desafio3/Main.java
+++ src/NivelIntermediario/desafio3/Main.java
@@ -1,4 +1,4 @@
-package NivelIntermediario;
+package NivelIntermediario.desafio3;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/NivelIntermediario/desafio3/Uzumaki.java
@@ -0,0 +1,10 @@
+package NivelIntermediario.desafio3;
+
+public class Uzumaki extends Ninja {
+
+    public void ModoSabioAtivado() {
+
+        System.out.println("Meu é " + nome + ". E eu ativei o modo Sabio"  );
+    }
+
+}
