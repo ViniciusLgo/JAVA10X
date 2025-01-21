766414142e33f4cff441e0f269e52ecf75a317be
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-01-21 16:52:27 -0300

    get and set

--- src/NivelIntermediario/Ninja.java
+++ src/NivelIntermediario/Ninja.java
@@ -5,72 +5,50 @@ public abstract class Ninja implements EstrategiaDeBatalha {
     //TODO: Incluir novos 2 atributos: numeroDeMissoesConcluidas , Rank
     // TODO: Rank: Gennin, Chunnin, Jounnin, Hokage
 
-    String nome;
-    String aldeia;
-    int idade;
-    int numeroDeMissoesConcluidas;
-    NivelNinja rank;
+    private String nome;
+    private String aldeia;
+    private int idade;
+    private int numeroDeMissoesConcluidas;
+    private NivelNinja rank;
 
-    public Ninja() {
+
+    public String getNome() {
+        return nome;
     }
 
-    // Metodo existente: Primeiro metodo
-    public Ninja(String nome, String aldeia, int idade) {
+    public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getAldeia() {
+        return aldeia;
+    }
+
+    public void setAldeia(String aldeia) {
         this.aldeia = aldeia;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
         this.idade = idade;
     }
 
-    // TODO: Sobrecarga do construtor chamado os novos atributos
-    // Sobrecarga de metodos voce nao precisa redeclarar o construtor so os novos atributos
-    public Ninja(String nome, String aldeia, int idade, int numeroDeMissoesConcluidas, NivelNinja rank) {
-        this(nome, aldeia, idade);
+    public int getNumeroDeMissoesConcluidas() {
+        return numeroDeMissoesConcluidas;
+    }
+
+    public void setNumeroDeMissoesConcluidas(int numeroDeMissoesConcluidas) {
         this.numeroDeMissoesConcluidas = numeroDeMissoesConcluidas;
+    }
+
+    public NivelNinja getRank() {
+        return rank;
+    }
+
+    public void setRank(NivelNinja rank) {
         this.rank = rank;
     }
-
-
-    // Metodos geral! Todos os ninjas vao ter
-    public void habilidadeEspecial() {
-        System.out.println("Meu nome é " + nome + " e esse é meu ataque especial");
-    }
-
-    // Sobreescrevendo o metodo da interface
-    @Override
-    public void estrategiaDeBatalhaNinja() {
-        System.out.println( "Meu nome é: " + nome + " Essa é minha ESTRATEGIA de combate");
-    }
-
-    // Inteligencia de combate - Metodo padrao
-    public void inteligenciaDeCombate() {
-        System.out.println("Meu nome é: " + nome + " Essa é minha INTELIGENCIA de combate");
-    }
-
-    // Inteligencia de combate -  Sobrecarga de metodo
-    public void inteligenciaDeCombate(int qi) {
-
-        if (qi > 150) {
-            System.out.println("Seu QI é: " + qi + " e voce é um genio");
-        } else if ( qi >= 130 ) {
-            System.out.println("Seu QI é: " + qi + " e voce é um ninja promissor");
-        } else {
-            System.out.println("Seu QI é: " + qi + " e voce precisa treinar mais suas estrategias");
-        }
-
-    }
-
-    // Metodo sem @override
-    public void metodoProvisorio(){
-
-    }
-
-
-    @Override
-    public String toString() {
-        return "esse é o metodo toString para ref de memoria!";
-    }
-
-
-
-
 }
