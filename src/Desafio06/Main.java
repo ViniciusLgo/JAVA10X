b229896b113ac7bdb0240285e9e2b6e014b84996
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-01-31 00:21:03 -0300

    exercises e desafio 06

--- src/Desafio06/Main.java
+++ src/Desafio06/Main.java
@@ -1,44 +1,88 @@
 package Desafio06;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Ninja> genins = new ArrayList<>();
 
-/*
+        // Adicionando Genins à lista
+        genins.add(new Ninja("Naruto", 25, 55));
+        genins.add(new Ninja("Sasuke", 26, 50));
+        genins.add(new Ninja("Sakura", 25, 45));
+        genins.add(new Ninja("Kakashi", 35, 60));
+        genins.add(new Ninja("Itachi", 27, 58));
+        genins.add(new Ninja("Gaara", 24, 52));
 
-        LinkedList<Ninja> listDeNinjas = new LinkedList<>();
-
-        listDeNinjas.add(new Ninja("Naruto Uzumaki", 17, "Konoha"));
-        listDeNinjas.add(new Ninja("Sasuke Uchiha", 17, "Konoha"));
-        listDeNinjas.add(new Ninja("Sakura Haruno", 17, "Konoha"));
-        listDeNinjas.add(new Ninja("Kakashi Hatake", 31, "Konoha"));
-        listDeNinjas.add(new Ninja("Gaara", 17, "Areia"));
-        listDeNinjas.add(new Ninja("Shikamaru Nara", 17, "Konoha"));
-        listDeNinjas.add(new Ninja("Temari", 18, "Areia"));
-*/
-
-        ArrayList<Ninja> arrayDeNinjas= new ArrayList<>();
-
-        arrayDeNinjas.add(new Ninja("Naruto",25,55));
-        arrayDeNinjas.add(new Ninja("Naruto", 25, 55));
-        arrayDeNinjas.add(new Ninja("Sasuke", 26, 50));
-        arrayDeNinjas.add(new Ninja("Sakura", 25, 45));
-        arrayDeNinjas.add(new Ninja("Kakashi", 35, 60));
-        arrayDeNinjas.add(new Ninja("Itachi", 27, 58));
-        arrayDeNinjas.add(new Ninja("Gaara", 24, 52));
-
-        System.out.println("-------- Lista de Ninjas -------");
-        for (Ninja ninja : arrayDeNinjas) {
-            System.out.println(ninja);
+        // Exibir todos os Genins cadastrados
+        System.out.println("-------- Lista de Genins -------");
+        for (Ninja genin : genins) {
+            System.out.println(genin);
         }
 
+        // Encontrar e exibir o Genin com a melhor nota
+        Ninja melhorGenin = encontrarMelhorGenin(genins);
+        System.out.println("\nO Genin com a melhor nota é: " + melhorGenin);
 
+        // Remover um Genin pelo nome
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nDigite o nome do Genin que deseja remover: ");
+        String nomeParaRemover = scanner.nextLine();
 
+        boolean removido = removerGenin(genins, nomeParaRemover);
+        if (removido) {
+            System.out.println("\nGenin removido com sucesso!");
+        } else {
+            System.out.println("\nGenin não encontrado.");
+        }
 
+        // Exibir a lista após a remoção
+        System.out.println("\n-------- Lista de Genins Atualizada -------");
+        for (Ninja genin : genins) {
+            System.out.println(genin);
+        }
 
+        scanner.close();
+    }
 
+    // Método para encontrar o Genin com a melhor nota
+    public static Ninja encontrarMelhorGenin(ArrayList<Ninja> lista) {
+        Ninja melhor = lista.get(0); // Assume que o primeiro Genin tem a melhor nota inicialmente
 
+        for (Ninja genin : lista) {
+            if (genin.getNota() > melhor.getNota()) {
+                melhor = genin; // Atualiza se encontrar um Genin com nota maior
+            }
+        }
+        return melhor;
+    }
 
+    // Método para remover um Genin pelo nome
+    public static boolean removerGenin(ArrayList<Ninja> lista, String nome) {
+        Iterator<Ninja> iterator = lista.iterator();
+        while (iterator.hasNext()) {
+            Ninja genin = iterator.next();
+            if (genin.getNome().equalsIgnoreCase(nome)) {
+                iterator.remove();
+                return true; // Retorna true se encontrou e removeu o Genin
+            }
+        }
+        return false; // Retorna false se o Genin não foi encontrado
     }
 }
+
+
+/*
+Descrição:Crie um sistema para armazenar e gerenciar genins em uma ArrayList.
+ Cada genin tem um nome, idade e nota na academia ninja. O sistema deverá:
+
+Adicionar 5 genins à lista.
+
+Exibir todos os genins cadastrados.
+
+Encontrar e exibir o genin com a melhor nota.
+
+Remover um genin pelo nome.
+*/
--- src/Desafio06/Ninja.java
+++ src/Desafio06/Ninja.java
@@ -1,19 +1,18 @@
 package Desafio06;
 
-import java.util.LinkedList;
-
 public class Ninja {
-
     private String nome;
     private int idade;
     private int nota;
 
-
-    @Override
-    public String toString() {
-        return "Ninja" + nome +", idade=" + idade + ", nota=" + nota;
+    // Construtor
+    public Ninja(String nome, int idade, int nota) {
+        this.nome = nome;
+        this.idade = idade;
+        this.nota = nota;
     }
 
+    // Getters e Setters
     public String getNome() {
         return nome;
     }
@@ -38,51 +37,9 @@ public class Ninja {
         this.nota = nota;
     }
 
-    public Ninja() {
+    // Exibição formatada
+    @Override
+    public String toString() {
+        return "Genin: " + nome + " | Idade: " + idade + " | Nota: " + nota;
     }
-
-    public Ninja(String nome, int idade, int nota) {
-        this.nome = nome;
-        this.idade = idade;
-        this.nota = nota;
-    }
-
-
-    /* private String nome;
-    private int idade;
-    private String aldeia;
-
-    public Ninja(String nome, int idade, String aldeia) {
-        this.nome = nome;
-        this.aldeia = aldeia;
-        this.idade = idade;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getAldeia() {
-        return aldeia;
-    }
-
-    public void setAldeia(String aldeia) {
-        this.aldeia = aldeia;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }*/
-
-
-
 }
